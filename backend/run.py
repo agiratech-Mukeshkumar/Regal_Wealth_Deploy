acf5ab0454632d26ae27136a708d5e5e97406292
agiratech-Mukeshkumar <mukesh.r@agiratech.com>
2025-09-10 16:57:08 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -31,4 +31,4 @@ def index():
     return "Regal Wealth Advisors API is running."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
